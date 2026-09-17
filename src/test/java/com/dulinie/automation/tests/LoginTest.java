2e5d225fa1cbfb9b27a05e32723509d07b6775cc
dulinie <dulinie@gmail.com>
2026-09-16 22:37:28 -0700

    Removed lass level shared page variables to prevent thread wire-crossing

--- src/test/java/com/dulinie/automation/tests/LoginTest.java
+++ src/test/java/com/dulinie/automation/tests/LoginTest.java
@@ -16,25 +16,29 @@ public class LoginTest extends BaseTest {
     @BeforeMethod
     public void initializePage() {
         // Initialize the page object once before every test method execution
-        loginPage = new LoginPage();
-        dashboardPage= new DashboardPage();
+        //loginPage = new LoginPage();
+       // dashboardPage= new DashboardPage();
 
     }
 
     @Test(priority = 1,description = "Verify that the OrangeHRM logo is displayed on the login page")
     public void verifyLogoDisplay() {
+        loginPage = new LoginPage();
         boolean isLogoDispayed = loginPage.isLogoDisplayed();
         Assert.assertTrue(isLogoDispayed,"OrangeHRM logo was not displayed on the login page.");
     }
 
     @Test(priority = 2, description = "Verify that the login page title is correct")
     public void verifyLoginPageTitle(){
+        loginPage = new LoginPage();
         String pageTitle = loginPage.validateLoginPageTitle();
         Assert.assertEquals(pageTitle,"OrangeHRM", "The login page title does not match.");
     }
 
     @Test(priority = 3, description = "Verify successful login to the application ")
     public void verifyLogin(){
+        loginPage = new LoginPage();
+        dashboardPage= new DashboardPage();
 
         loginPage.loginWithDefaultCredentials();
         boolean isDashboardHeaderVisible = dashboardPage.isDashboardHeaderDisplayed();
