20f7c256291778f86d9cf5da49ee7e07bb13b159
dulinie <dulinie@gmail.com>
2026-09-16 17:21:34 -0700

    Implemented the GitHub Actions workflow for automated testing

--- src/main/java/com/dulinie/automation/driver/DriverManager.java
+++ src/main/java/com/dulinie/automation/driver/DriverManager.java
@@ -5,9 +5,12 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import com.dulinie.automation.config.ConfigManager;
+import org.openqa.selenium.firefox.FirefoxOptions;
 
 
 public class DriverManager {
@@ -30,25 +33,60 @@ public class DriverManager {
     }
 
     public static void initializeDriver() {
+
+        // Hide verbose Selenium standard logger outputs from crowding your pipeline logs
+        java.util.logging.Logger.getLogger("org.openqa.selenium").setLevel(java.util.logging.Level.SEVERE);
+
         String browser = ConfigManager.getConfig().browser();
         String url = ConfigManager.getConfig().url();
 
+        // Check if headless mode is requested via command line parameter (-Dheadless=true)
+        boolean isHeadless = System.getProperty("headless", "false").equalsIgnoreCase("true");
 
         if(browser.equalsIgnoreCase("chrome")) {
             WebDriverManager.chromedriver().setup();
-            setDriver(new ChromeDriver());
+
+            ChromeOptions options = new ChromeOptions();
+            if (isHeadless) {
+                options.addArguments("--headless=new");
+                options.addArguments("--disable-gpu");
+                options.addArguments("--window-size=1920,1080");
+                options.addArguments("--no-sandbox");
+                options.addArguments("--disable-dev-shm-usage");
+            }
+            setDriver(new ChromeDriver(options));
+
         }else if (browser.equalsIgnoreCase("firefox")) {
             WebDriverManager.firefoxdriver().setup();
-            setDriver(new FirefoxDriver());
+            FirefoxOptions options = new FirefoxOptions();
+            if (isHeadless) {
+                options.addArguments("--headless");
+                options.addArguments("--window-size=1920,1080");
+            }
+            setDriver(new FirefoxDriver(options));
+
         } else if (browser.equalsIgnoreCase("edge")) {
             WebDriverManager.edgedriver().setup();
-            setDriver(new EdgeDriver());
+            EdgeOptions options = new EdgeOptions();
+            if (isHeadless) {
+                options.addArguments("--headless=new");
+                options.addArguments("--disable-gpu");
+                options.addArguments("--window-size=1920,1080");
+                options.addArguments("--no-sandbox");
+                options.addArguments("--disable-dev-shm-usage");
+            }
+            setDriver(new EdgeDriver(options));
+
         }
         else
             throw new RuntimeException("Unsupported browser specified in config: " + browser);
 
-        getDriver().manage().window().maximize();
-        getDriver().get(url);
+        // Window maximizing only takes functional effect if the session is visual
+        if (!isHeadless) {
+            getDriver().manage().window().maximize();
+        }
+
+              getDriver().get(url);
 
     }
 
